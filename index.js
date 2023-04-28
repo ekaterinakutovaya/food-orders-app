820f84f3ee8bca7781237044db51538458e9ca29
ekaterinakutovaya <e.katerina.kutovaya@yandex.ru>
2023-04-28 17:11:45 +0500

    index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(cors());
 
 app.get('/', (req, res) => res.sendFile(path.resolve('dist', 'index.html')));
 // Initialize server
-app.listen(3000, () => {
-    console.log("Running on port 3000.");
+app.listen(5000, () => {
+    console.log("Running on port 5000.");
 });
 
 module.exports = app;
